fff5bdaca3708d4166d1561a77b6f5b9b8930373
Rodrigo Castillo <rcastillo159@gmail.com>
2021-05-21 19:15:15 -0400

    21-5-21 19:15

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bienvenida-a-Telegram
+Aquí se muestra el código del bot @ñiñiñi_bot
+
+Este bot de telegram reemplaza toda vocal por la i
--- indicadores.py
+++ indicadores.py
@@ -2,16 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver = webdriver.Chrome(r"C:\Users\PC1337\Desktop\Codigo Curso Python\chromedriver.exe")
+options = Options()
+options.headless = True
+
+driver = webdriver.Chrome(r".\chromedriver.exe", chrome_options=options)
 
 driver.get('https://bancosantanderinversiones.finmarketslive.cl/www/mercados.html')
 
 wait = WebDriverWait(driver, 10)
 wait.until(presence_of_element_located((By.ID, "Wrapper")))
 
-while True:        
+def main():
     ipsa = driver.find_elements_by_xpath("//tr[@id='Edom:2,i:3969,q:1,c:numerito_instrument_2']/td[position()<=2]")
     for indicador in ipsa:
         print(indicador.text)
@@ -32,5 +36,8 @@ while True:
     for indicador in oro:
         print(indicador.text)
     print("----------")
+
+while True:
+    main()
     time.sleep(5)
 
