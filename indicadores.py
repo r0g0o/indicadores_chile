36712be4b53bcf0d0c487f5e4ff13d97390c1d91
Rodrigo Castillo <rcastillo159@gmail.com>
2021-05-26 18:28:13 -0400

    18:28

--- indicadores.py
+++ indicadores.py
@@ -19,8 +19,9 @@ import time
 
 options = Options()
 options.headless = True
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-driver = webdriver.Chrome('chromedriver.exe', chrome_options=options)
+driver = webdriver.Chrome('./chromedriver.exe', options=options)
 
 driver.get('https://bancosantanderinversiones.finmarketslive.cl/www/mercados.html')
 
@@ -30,88 +31,60 @@ wait.until(presence_of_element_located((By.ID, "Wrapper")))
 def start(update, context):
     update.message.reply_text('Se ha iniciado el reporte de indicadores cada 5 segundos.')
     while True:
+        mensaje = []
+        mensaje1 = ""
+        mensaje2 = ""
+        mensaje3 = ""
+        mensaje4 = ""
+        mensaje5 = ""
         ipsa = driver.find_elements_by_xpath("//tr[@id='Edom:2,i:3969,q:1,c:numerito_instrument_2']/td[position()<=2]")
         for indicador in ipsa:
-            update.message.reply_text(indicador.text)
+            mensaje1 = mensaje1 + ": " + indicador.text
         print()
         cobre = driver.find_elements_by_xpath("//tr[@id='Edom:9,i:393,q:2,c:numerito_instrument_2']/td[position()<=2]")
         for indicador in cobre:
-            update.message.reply_text(indicador.text)
-        print()
+            mensaje2 = mensaje2 + ": " + indicador.text
+        mensaje.append(mensaje2)
         dolar = driver.find_elements_by_xpath("//tr[@id='Edom:14,i:2,q:2,c:numerito_instrument_4']/td[position()<=2]")
         for indicador in dolar:
-            update.message.reply_text(indicador.text)
-        print()
+            mensaje3 = mensaje3 + ": " + indicador.text
+        mensaje.append(mensaje3)
         wti = driver.find_elements_by_xpath("//tr[@id='Edom:10,i:982,q:2,c:numerito_instrument_2']/td[position()<=2]")
         for indicador in wti:
-            update.message.reply_text(indicador.text)
-        print()
+            mensaje4 = mensaje4 + ": " + indicador.text
+        mensaje.append(mensaje4)
         oro = driver.find_elements_by_xpath("//tr[@id='Edom:11,i:980,q:2,c:numerito_instrument_2']/td[position()<=2]")
         for indicador in oro:
-            update.message.reply_text(indicador.text)
+            mensaje5 = mensaje5 + ": " + indicador.text
+        mensaje.append(mensaje5)
+        print(mensaje)
+        for i in mensaje:
+            update.message.reply_text(i.replace(": ", "", 1))
         update.message.reply_text("-------------")
         time.sleep(5)
-
-'''
-def help(update, context):
-    update.message.reply_text('Help!')
-
-
-def echo(update, context):
-    """Reemplaza las vocales por la i, ya sea minúscula, mayuscola, con o sin tilde"""
-
-    texto1 = update.message.text
-
-    texto2 = texto1.replace("a", "i").replace("e", "i").replace("o", "i").replace("u", "i")
-
-    texto3 = texto2.replace("A", "I").replace("E", "I").replace("O", "I").replace("U", "I")
-
-    texto4 = texto3.replace("á", "í").replace("é", "í").replace("ó", "í").replace("ú", "í")
-
-    texto5 = texto4.replace("Á", "Í").replace("É", "Í").replace("Ó", "Í").replace("Ú", "Í")
-    
-    update.message.reply_text(texto5, quote=False)
-'''
 
 def prueba(update, context):
     update.message.reply_text('Hola mundo')
 
-
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-
 def main():
     updater = Updater(TOKEN, use_context=True)
-
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    
     dp.add_handler(CommandHandler("prueba", prueba))
     dp.add_handler(CommandHandler("start", start))
-    #dp.add_handler(CommandHandler("help", help))
-    '''
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-    '''
-    # log all errors
-    dp.add_error_handler(error)
 
-    # Start the Bot
+    dp.add_error_handler(error)
+    #updater.start_polling()
+    
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
     updater.bot.setWebhook('https://indicadoreschile.herokuapp.com/' + TOKEN)
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
-
 if __name__ == '__main__':
+    print("ejecutandose")
     main()
-
-#hola mundo
