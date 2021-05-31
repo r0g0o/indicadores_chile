e522145a3c8b5515313063db664fb51a0614111a
Rodrigo Castillo <rcastillo159@gmail.com>
2021-05-30 21:31:22 -0400

    21:31

--- indicadores.py
+++ indicadores.py
@@ -4,8 +4,7 @@ import os
 
 PORT = int(os.environ.get('PORT', 5000))
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +16,8 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 import time
+import datetime
+import asyncio
 
 options = Options()
 options.headless = True
@@ -24,72 +25,39 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 driver = webdriver.Chrome('./chromedriver.exe', options=options)
 
-driver.get('https://bancosantanderinversiones.finmarketslive.cl/www/mercados.html')
-#driver.get('https://mercadosenlinea.inversionessecurity.cl/www/resumen.html')
+driver.get('https://bancobci.finmarketslive.cl/www/index.html')
 
 wait = WebDriverWait(driver, 10)
-wait.until(presence_of_element_located((By.ID, "Wrapper")))
-#wait.until(presence_of_element_located((By.XPATH, "/html/body/div[4]/div")))
+wait.until(presence_of_element_located((By.XPATH, "/html/body/section/div/div[2]")))
 
 def start(update, context):
     update.message.reply_text('Se ha iniciado el reporte de indicadores cada 5 segundos.')
     while True:
-        mensaje = []
-        mensaje1 = ""
-        mensaje2 = ""
-        mensaje3 = ""
-        mensaje4 = ""
-        mensaje5 = ""
-        mensaje6 = ""
-        mensajef = []
+        mensaje = {"IPSA": "", "Dolar": "", "Cobre": "", "UF": ""}
+        mensaje_lista = []
+        valores = []
+        variaciones = []
+        xpath = ["/html/body/section/div/div[2]/div[2]/table/tbody/tr[1]", 
+        "/html/body/section/div/div[2]/div[1]/div/div[1]/div[2]/div[2]/table/tbody/tr[1]", 
+        "/html/body/section/div/div[2]/div[1]/div/div[1]/div[2]/div[3]/table[1]/tbody/tr[1]", 
+        "/html/body/section/div/div[2]/div[1]/div/div[1]/div[2]/div[2]/table/tbody/tr[4]"]  #IPSA, Dolar, Cobre y UF, respectivamente
+
+        for i in xpath: #agregar valor
+            valores.append(driver.find_element_by_xpath(i + "/td[2]/span").text)
+        for i in xpath: # agregar variación
+            variaciones.append(driver.find_element_by_xpath(i + "/td[3]/span[1]").text)
         
-        ipsa = driver.find_elements_by_xpath("//tr[@id='Edom:2,i:3969,q:1,c:numerito_instrument_2']/td")
-        for indicador in ipsa:
-            mensaje1 = mensaje1 + ": " + indicador.text
-        mensaje.append(mensaje1)
-        cobre = driver.find_elements_by_xpath("//tr[@id='Edom:9,i:393,q:2,c:numerito_instrument_2']/td")
-        for indicador in cobre:
-            mensaje2 = mensaje2 + ": " + indicador.text
-        mensaje.append(mensaje2)
-        dolar = driver.find_elements_by_xpath("//tr[@id='Edom:14,i:2,q:2,c:numerito_instrument_4']/td")
-        for indicador in dolar:
-            mensaje3 = mensaje3 + ": " + indicador.text
-        mensaje.append(mensaje3)
-        wti = driver.find_elements_by_xpath("//tr[@id='Edom:10,i:982,q:2,c:numerito_instrument_2']/td")
-        for indicador in wti:
-            mensaje4 = mensaje4 + ": " + indicador.text
-        mensaje.append(mensaje4)
-        oro = driver.find_elements_by_xpath("//tr[@id='Edom:11,i:980,q:2,c:numerito_instrument_2']/td")
-        for indicador in oro:
-            mensaje5 = mensaje5 + ": " + indicador.text
-        mensaje.append(mensaje5)
-        uf = driver.find_elements_by_xpath("//*[@id='Wrapper']/div[1]/div/div[2]/div/div/div[1]/ul/li[4]/div")
-        for indicador in uf:
-            mensaje6 = mensaje6 + ": " + indicador.text
-        mensaje.append(mensaje6)
-        '''
-        ipsa = driver.find_elements_by_xpath("//*[@id='Edom:237224065932,i:3969,q:1,c:pushNormalFilterIndices']")
-        for indicador in ipsa:
-            mensaje1 = mensaje1 + ": " + indicador.text
-        mensaje.append(mensaje1)
-        cobre = driver.find_elements_by_xpath("//*[@id='Edom:12953293362,i:393,q:1,c:pushFilterCommodities']")
-        for indicador in cobre:
-            mensaje2 = mensaje2 + ": " + indicador.text
-        mensaje.append(mensaje2)
-        dolar = driver.find_elements_by_xpath("//*[@id='Edom:195794128,i:2,q:2,c:pushFilterCurrencies']")
-        for indicador in dolar:
-            mensaje3 = mensaje3 + ": " + indicador.text
-        mensaje.append(mensaje3)
-        uf = driver.find_elements_by_xpath("/html/body/div[4]/div/div[2]/div[2]/div[6]/div[2]/div[1]")
-        for indicador in uf:
-            mensaje4 = mensaje4 + ": " + indicador.text
-        mensaje.append(mensaje4)
-        '''
         for i in mensaje:
-            mensajef.append(i.replace(": ", "", 1) + "\n")
-        print(mensajef)
-        update.message.reply_text("".join(mensajef))
-        update.message.reply_text("-------------")
+            x = 0
+            y = valores[x] + " " + variaciones[x]
+            mensaje[i] = y
+            x += 1
+
+        for i in mensaje:
+            mensaje_lista.append(i + " " + mensaje[i] + "\n")
+        ahora = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        mensaje_lista.insert(0, "Registro a las: " + ahora + "\n")
+        update.message.reply_text("".join(mensaje_lista))
         time.sleep(5)
 
 def prueba(update, context):
@@ -106,13 +74,13 @@ def main():
     dp.add_handler(CommandHandler("start", start))
 
     dp.add_error_handler(error)
-    updater.start_polling()
-    '''
-    updater.start_webhook(listen="0.0.0.0",
+
+    '''updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://indicadoreschile.herokuapp.com/' + TOKEN)
-    '''
+    updater.bot.setWebhook('https://indicadoreschile.herokuapp.com/' + TOKEN)'''
+
+    updater.start_polling()
     updater.idle()
 
 if __name__ == '__main__':
