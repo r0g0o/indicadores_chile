13e77efce8bf0210bffb96823cbef7a659626171
Rodrigo Castillo <rcastillo159@gmail.com>
2021-06-09 17:08:31 -0400

    17:08

--- indicadores.py
+++ indicadores.py
@@ -2,14 +2,6 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 
-PORT = int(os.environ.get('PORT', 5000))
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-TOKEN = os.getenv("TOKEN")
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
@@ -17,13 +9,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 import time
 import datetime
+import threading
+
+PORT = int(os.environ.get('PORT', 5000))
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+#TOKEN = os.getenv("TOKEN")
 
 options = Options()
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.headless = True
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no_sandbox")
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+#options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
 
 driver.get('https://bancobci.finmarketslive.cl/www/index.html')
@@ -31,9 +32,14 @@ driver.get('https://bancobci.finmarketslive.cl/www/index.html')
 wait = WebDriverWait(driver, 10)
 wait.until(presence_of_element_located((By.XPATH, "/html/body/section/div/div[2]")))
 
-def start(update, context):
-    update.message.reply_text('Se ha iniciado el reporte de indicadores cada 5 segundos.')
-    while True:
+iniciador = False
+
+def scrapping(update, context):
+    global iniciador
+    update.message.reply_text("Se ha iniciado el reporte de indicadores cada 5 segundos. Para detenerlo, escribe '/stop'")
+    reloj = time.gmtime(time.time())
+    while iniciador:
+        #if (reloj.tm_sec == 0) and (reloj.tm_min == 0) and (reloj.tm_hour == 10 + 4):#Cada día a las 10am
         mensaje = {"IPSA": "", "Dolar": "", "Cobre": "", "UF": ""}
         mensaje_lista = []
         valores = []
@@ -61,8 +67,23 @@ def start(update, context):
         update.message.reply_text("".join(mensaje_lista))
         time.sleep(5)
 
+
+def start(update, context):
+    global iniciador
+    iniciador = True
+    t = threading.Thread(target=scrapping, args=(update, context))
+    t.start()
+
 def prueba(update, context):
     update.message.reply_text('Hola mundo')
+
+def stop(update, context):
+    global iniciador
+    iniciador = False
+    update.message.reply_text("Se ha desactivado el reporte. Para volverlo a iniciar, escriba '/start'")
+
+def echo(update, context):
+    update.message.reply_text('echo')
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -73,17 +94,21 @@ def main():
 
     dp.add_handler(CommandHandler("prueba", prueba))
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("echo", echo))
+    dp.add_handler(CommandHandler("stop", stop))
 
     dp.add_error_handler(error)
-
+    '''
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
     updater.bot.setWebhook('https://indicadoreschile.herokuapp.com/' + TOKEN)
-
-    #updater.start_polling()
+    '''
+    updater.start_polling()
     updater.idle()
 
 if __name__ == '__main__':
     print("ejecutandose")
+    reloj = time.gmtime(time.time())
+    print(reloj.tm_hour)
     main()
