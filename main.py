bd6151c0c785e36c33e437c150c6c1af8fea2822
Rodrigo Castillo <53545808+r0g0o@users.noreply.github.com>
2021-11-13 22:26:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import solicitud
 from datetime import datetime
-import asyncio
 
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
@@ -12,9 +11,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-#TOKEN = os.getenv("TOKEN")
-TOKEN = "1827921985:AAFyMZYqk4bSQYlzfHbGFcaaYKz_N8kR0Bg"
-
+TOKEN = os.getenv("TOKEN")
 
 def prueba(update, context):
     update.message.reply_text('Hola mundo. Para mostrar los indicadores, escribe /muestra.')
@@ -31,18 +28,6 @@ def scrapping(update, context):
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-
-async def vuelta(update, context):
-    print("async_foo started")
-    while True:
-        update.message.reply_text(":)")
-        await asyncio.sleep(5)
-
-def go(update, context):
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(vuelta())
-
-
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
@@ -50,14 +35,13 @@ def main():
     dp.add_handler(CommandHandler("prueba", prueba))
     dp.add_handler(CommandHandler("start", prueba))
     dp.add_handler(CommandHandler("muestra", scrapping))
-    dp.add_handler(CommandHandler("vuelta", go))
 
     dp.add_error_handler(error)
     
-    #updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
-    #updater.bot.setWebhook('https://indicadoreschile.herokuapp.com/' + TOKEN)
+    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
+    updater.bot.setWebhook('https://indicadoreschile.herokuapp.com/' + TOKEN)
     
-    updater.start_polling()
+    #updater.start_polling()
     updater.idle()
 
 if __name__ == '__main__':
